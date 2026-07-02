f8b9877177d11321ea94ea2384d21d03468ae72a
Alexandria W. <96226175+Bitslayn@users.noreply.github.com>
2026-07-02 13:37:26 -0400

    PackageD v1.2b-rev4

--- Utilities/PackageD.lua
+++ Utilities/PackageD.lua
@@ -32,7 +32,7 @@ local _FOX = setmetatable({}, { __index = _G })
 ---@param level integer
 ---@return string, string
 local function get_script(level)
-	return select(2, pcall(function() error("", level + 3) end)):match(("%q"):format(prefix):sub(2, -2) .. "(.-)/?([^/]+):")
+	return select(2, pcall(function() error("", level + 3) end)):match(prefix:gsub("%A", "%%%1") .. "(.-)/?([^/]+):")
 end
 
 ---Unpacks the current traceback environment onto the navigation table
